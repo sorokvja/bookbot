0f1ca5943b0df346736381c0ced19a5bbca65a00
sorokvja <vjacheslav.sorokin@gmail.com>
2025-03-13 06:14:27 +0200

    add dynamic file path support

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-def get_book_text(filepath):
-    file_contents = ""
-    with open(filepath, 'r') as file:
-        file_contents = file.read()
-        #file.closed
-    return file_contents
-
+import sys 
 from stats import count_words
 from stats import count_chars
 from stats import sort_chars 
 
+def get_book_text(filepath):
+    file_contents = ""
+    with open(filepath, 'r') as file:
+        file_contents = file.read()
+    return file_contents
+
 def main():
-    book_text = get_book_text('./books/frankenstein.txt')
+    #book_text = get_book_text('./books/frankenstein.txt')
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
     char_dictionary = count_chars(book_text)
     sorted_chars = sort_chars(char_dictionary)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    #print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {count_words(book_text)} total words")
     print("--------- Character Count -------")
